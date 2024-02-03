37b699c2f62acc1bb14f893285a6b1d3981dde0b
Jonatan Puma <148421385+pjonatan900@users.noreply.github.com>
2024-02-02 21:43:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import streamlit as st
-import pandas as pd
+import streamlit as st 
+import pandas as pd 
 import numpy as np
-st.title('UPC DATA SCIENCE')
+
+st.title('UPC Data Science 2024')
+st.header('Simulador Ventas')
+n = st.slider("cant. ventas", 5,100, step=1)
+chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
+st.line_chart(chart_data)
